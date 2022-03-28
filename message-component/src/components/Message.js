1838916f0ccd56c426c80d223f6c1b67adf342b1
JakubP1 <jack.penkala99@gmail.com>
2022-03-28 18:57:07 +0200

    Adding component visited state

--- message-component/src/components/Message.js
+++ message-component/src/components/Message.js
@@ -1,23 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Message = () => {
+  const [isVisited, setIsVisited] = useState(false);
+  const [vistiedText, setVisitedText] = useState("Nesplneno");
+  const [checked, setChecked] = useState(false);
+
+  const isVisitedClickHandler = () => {
+    setIsVisited(true);
+    setChecked(true);
+    setVisitedText("Splneno");
+  };
+
   return (
-    // <div className="card" style="width: 18rem;">
-    //   <div className="card-body">
-    //     <h5 className="card-title">Card title</h5>
-    //     <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    //     <p className="card-text">
-    //       Some quick example text to build on the card title and make up the
-    //       bulk of the card's content.
-    //     </p>
-    //     <a href="#" className="card-link">
-    //       Card link
-    //     </a>
-    //     <a href="#" className="card-link">
-    //       Another link
-    //     </a>
-    //   </div>
-    //  </div>
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
@@ -26,7 +20,7 @@ const Message = () => {
           bulk of the card's content.
         </p>
         <span style={{ paddingRight: "10px" }}> Odkaz: </span>
-        <a href="#" className="card-link">
+        <a href="#" className="card-link" onClick={isVisitedClickHandler}>
           www.bla.la
         </a>
         <div className="input-group mb-3">
@@ -35,10 +29,15 @@ const Message = () => {
               <input
                 type="checkbox"
                 aria-label="Checkbox for following text input"
+                checked={checked}
               />
             </div>
           </div>
-          <span className="badge alert-danger">Nesplneno</span>
+          <span
+            className={`badge ${isVisited ? "alert-success" : "alert-danger"}`}
+          >
+            {vistiedText}
+          </span>
         </div>
       </div>
     </div>
